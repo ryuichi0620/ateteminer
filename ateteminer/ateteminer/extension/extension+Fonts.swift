e83806477e936372be2bd6acabdb33e7cb0abaef
egoshi <ryuichi062000@gmail.com>
2024-06-10 21:44:19 +0900

    add: カスタムフォントを設定

---
+++ ateteminer/ateteminer/extension/extension+Fonts.swift
@@ -0,0 +1,13 @@
+//
+//  extension+Fonts.swift
+//  ateteminer
+//
+//  Created by 江越瑠一 on 2024/06/10.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static let dotSerif = Font.custom("DotGothic16-Regular", size: 15)
+}
