f65772b09102d1005c2dc290bc1a57883c9b1ece
egoshi <ryuichi062000@gmail.com>
2024-06-10 22:42:48 +0900

    imageのstaticを追加

--- ateteminer/ateteminer/ContentView.swift
+++ ateteminer/ateteminer/ContentView.swift
@@ -11,14 +11,9 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
         VStack {
-            
+            Image(.genie, bundle: nil)
         }
     }
     
---
+++ ateteminer/ateteminer/extension/extension+Image.swift
@@ -0,0 +1,15 @@
+//
+//  extension+Image.swift
+//  ateteminer
+//
+//  Created by 江越瑠一 on 2024/06/10.
+//
+
+import Foundation
+import SwiftUI
+
+extension Image {
+    static var genie: Image {
+        Image(uiImage: UIImage(named: "genie") ?? UIImage())
+    }
+}
