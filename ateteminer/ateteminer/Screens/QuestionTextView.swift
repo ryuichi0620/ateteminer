2b53fabb0efac3a9b626fd68b6e27a396725a6d5
egoshi <ryuichi062000@gmail.com>
2024-06-18 07:52:58 +0900

    add: 質問文を表示するViewを作成

---
+++ ateteminer/ateteminer/Screens/QuestionTextView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionTextView.swift
+//  ateteminer
+//
+//  Created by 江越瑠一 on 2024/06/18.
+//
+
+import SwiftUI
+
+struct QuestionTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionTextView()
+}
